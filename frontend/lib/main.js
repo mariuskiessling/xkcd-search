5be3a234b37308e68cbeccd9493f8c2b9f496cc8
Marius Kießling <vcs@mariuskiessling.de>
2018-10-26 17:54:22 +0200

    Add vue.js (+ axios) and support for API communication

---
+++ frontend/lib/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    query: '',
+    results: ''
+  },
+  methods: {
+    search: function () {
+      axios.get('http://127.0.0.1:5000/search/' + this.query).then(response => this.results = response.data);
+    }
+  }
+});
