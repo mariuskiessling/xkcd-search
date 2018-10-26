ceaa83117f60fbe17326deaa96be43f6f922ec71
Marius Kießling <vcs@mariuskiessling.de>
2018-10-26 20:45:55 +0200

    Add dynamic search and display of results

--- frontend/_javascript/main.js
+++ frontend/_javascript/main.js
@@ -2,13 +2,26 @@ var app = new Vue({
   el: '#app',
   data: {
     query: '',
-    results: ''
+    results: [],
+    loading: false,
+    searchPerformed: false
   },
   methods: {
     search: function() {
+      this.loading = true;
       axios
         .get('http://127.0.0.1:5000/search/' + this.query)
-        .then(response => (this.results = response.data))
+        .then(response => {
+          this.loading = false;
+          this.results = response.data;
+          this.searchPerformed = true;
+        })
+        .catch(error => {
+          console.log(error);
+          alert("An error occurred while trying to process your search. Please make sure that the API ist started and is functioning properly.");
+          this.loading = false;
+          this.searchPerformed = false;
+        });
     }
   }
 })
