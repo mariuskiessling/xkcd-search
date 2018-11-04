440029f8f23581b54047ac09434a88001a1b11bd
Marius Kießling <vcs@mariuskiessling.de>
2018-11-04 17:37:49 +0100

    Add hive operations to workflow

--- workflow/xkcd-search.py
+++ workflow/xkcd-search.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.hive_operator import HiveOperator
 import datetime
 
 default_args = {
@@ -71,5 +72,35 @@ placeComicFile = BashOperator(
     params={'runDate': runDate},
     dag=dag)
 
+dropHiveDatabase = HiveOperator(
+    task_id='stage2_drop_hive_db',
+    hql='DROP TABLE IF EXISTS xkcd_comics;',
+    hive_cli_conn_id='hive_cli_default',
+    dag=dag)
+
+createHiveSchema = HiveOperator(
+    task_id='stage2_create_hive_schema',
+    hql="""
+        CREATE EXTERNAL TABLE IF NOT EXISTS xkcd_comics(
+        month INT,
+        num INT,
+        link STRING,
+        year INT,
+        news STRING,
+        safe_title STRING,
+        transcript STRING,
+        alt STRING,
+        img STRING,
+        title STRING,
+        day int)
+        COMMENT 'XKCD comics'
+        ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+        STORED AS TEXTFILE LOCATION '/user/hadoop/xkcd-search/raw/{{ execution_date.year }}/{{ execution_date.day }}/{{ execution_date.month }}/{{ execution_date.hour }}-{{ execution_date.minute }}-{{ execution_date.second }}';
+        """,
+    hive_cli_conn_id='hive_cli_default',
+    dag=dag)
+
 createRunDateDir.set_upstream(createHdfsBase)
 placeComicFile.set_upstream(createRunDateDir)
+dropHiveDatabase.set_upstream(placeComicFile)
+createHiveSchema.set_upstream(dropHiveDatabase)
